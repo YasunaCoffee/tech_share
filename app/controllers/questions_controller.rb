966c357e26447e980b2a65ad9182d53075138124
yasunamiura <yasuna.miura@di-v.co,.jp>
2019-11-24 16:37:20 +0900

    add-show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,9 @@
 class QuestionsController < ApplicationController
   def index
+    @question = Question.find(1)
+  end
+
+  def show
+    @question = Question.find(1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'questions#index'
+  resources :questions
 end
