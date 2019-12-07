e1c66c7240bc61b9ef50595eb5d9c6bf85feb202
inoueyuuki <inoueyuuki@yuukinoMacBook-Pro.local>
2019-12-07 15:20:10 +0900

    make new files

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,35 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article, only:[:show]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    article = Article.new(article_params)
+    if article.save!
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :tag)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+end
