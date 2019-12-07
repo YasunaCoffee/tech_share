67a180b77dbfeb09619f16d8c9bc1419eb0f751a
inoueyuuki <inoueyuuki@yuukinoMacBook-Pro.local>
2019-12-07 15:27:38 +0900

    create articles table

---
+++ db/migrate/20191207050515_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :title, null: false
+      t.text :content, null: false
+      t.string :tag
+      t.timestamps
+    end
+  end
+end
