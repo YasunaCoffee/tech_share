23b9c1e9bf6d046284d2abe9a3d42efc3aa7508e
inoueyuuki <inoueyuuki@yuukinoMacBook-Pro.local>
2019-12-13 17:51:21 +0900

    edit question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,11 @@
 class Question < ApplicationRecord
   has_many :answers
-  #belongs_to :user
+  # belongs_to :user
+  # このアソシエーションがエラーの原因
+  # belongs_to :curriculum
   enum curriculum: {
     Basiccurriculum: 0,
     Applicationcurriculum: 1,
     Finalsubject: 2
   }
-  belongs_to :curriculum
 end
