461fecb606dcf71c5025f892a9d61c729bee0696
ratovia <ratovia@hotmail.co.jp>
2019-12-28 16:17:25 +0900

    fix end of input error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,6 +31,8 @@ class ArticlesController < ApplicationController
     else
       render :edit
     end
+  end
+  
   def search
     @articles = Article.where("title LIKE(?)", "#{params[:search]}%")
   end
