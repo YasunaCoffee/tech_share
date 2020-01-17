cba2de6f16c5e45ef575c6f3d741c9021f063f38
yasunamiura <yasuna.miura@di-v.co,.jp>
2020-01-17 11:06:36 +0900

    add-credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username ==Rails.application.credentials[:basic][:user_name] && password == Rails.application.credentials[:basic][:password]
+    end
+  end
 end
