8f7724dcb20af3dfbcc3fb1bdfe79cf273ea8f4e
yasunamiura <yasuna.miura@di-v.co,.jp>
2020-01-25 15:59:21 +0900

    create-tags

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,17 @@
+class TagsController < ApplicationController
+
+  def new
+    @tags = Tag.all
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.create(tag_params)
+    redirect_to root_path
+  end
+
+  private
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
+end
