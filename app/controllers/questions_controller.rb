c979206ad0352d2dba856ec5c18b1ce8daff72d9
inoueyuuki <inoueyuuki@yuukinoMacBook-Pro.local>
2020-02-05 00:51:12 +0900

    add before_action question_controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only:[:show, :edit, :update]
+  skip_before_action :authenticate_user!, only:[:index, :show, :search]
 
   def index
     @questions = Question.all
