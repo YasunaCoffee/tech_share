8f0eaa0d9cb7053654565c350664b1a990b1b229
Yasuna Miura <yasuna@localhost.localdomain>
2020-02-04 23:31:12 +0900

    create-comment-function

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -1,11 +1,11 @@
 class ArticleCommentsController < ApplicationController
     def create
       comment = ArticleComment.create(comment_params)
-      redirect_to "/articles/#{comment.article.id}" 
+      redirect_to "/articles/#{comment.article_id}" 
     end
   
     private
     def comment_params
-      params.require(:comment).permit(:content).merge( article_id: params[:article_id])
+      params.require(:article_comment).permit(:content).merge(article_id: params[:article_id])
     end
   end
