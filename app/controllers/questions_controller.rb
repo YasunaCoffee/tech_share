49a49a6dd7c9535efee497d3ba64152731a85b03
yasunamiura <yasuna.miura@di-v.co,.jp>
2020-02-21 14:34:49 +0900

    change-redirect-path

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,7 +19,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
-      redirect_to root_path
+      redirect_to questions_path
     else
       render :new
     end
