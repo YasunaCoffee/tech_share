454f5571e9e97919634fdfae9cabd1a9ef324665
yasunamiura <yasuna.miura@di-v.co,.jp>
2020-02-21 18:58:32 +0900

    setting-default-generates-files

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,12 @@ module TechShare
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
