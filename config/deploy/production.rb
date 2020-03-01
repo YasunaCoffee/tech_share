7a907ec28842ed9e4fc96b6c7d468ea7d6a370c3
Kazushi Yamanishi <keironia187149@gmail.com>
2020-03-01 11:43:00 +0900

    update ec2 settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '13.230.200.15', user: 'ec2-user', roles: %w{app db web}
+server '18.180.48.135', user: 'ec2-user', roles: %w{app db web}
 
 
 
