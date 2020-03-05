89a33d34e245a6657ee66d8ff2ecc2f396b89145
fuga-naito23 <53104142+fuga-naito23@users.noreply.github.com>
2020-03-05 15:44:32 +0900

    incremental_search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,58 @@
-$(function() {
+$(function () {
+
+  // HTML要素をコピーして貼り付ける
+  const buildArticle = function (article) {
+    let tags = ''
+      article.tags.forEach(function (tag) {
+        tag_name = ' ' + tag.name;
+        tags += tag_name
+      });
+
+    const html = `<div class="alert alert-dark" role="alert">
+    <div class="d-flex justify-content-between">
+    <h5 class="alert-heading">
+    <div class="fas fa-book" aria-hidden="true"></div>
+    <a class="alert-link" href="/articles/${article.id}">${article.title}
+    </a></h5>
+    <p>
+    投稿者：
+    ${article.user_name}
+    </p>
+    </div>
+    <hr>
+    ${tags}
+    </div>`
+    return html
+  }
+  const buildQuestions = function (question) {
+    console.log(question)
+
+    const html = `<div class="alert alert-warning">
+    <h5 class="alert-heading">
+    <div class="fas fa-question-circle" aria-hidden="true">
+    <a class="alert-link" href="/questions/${question.id}">${question.title}
+    </a></div>
+    </h5>
+    <hr>
+    <div class="d-flex justify-content-between">
+    <div class="name">
+    質問者：
+    ${question.user_name}
+    </div>
+    <div class="date">
+    投稿日時：
+    ${question.created_at}
+    </div>
+    </div>
+    
+    </div>`
+    return html
+  }
+
+  // 検証からコピペできる
+  // フロントの検索ボタンと、検索条件のボタンを削除する
   
-  $('.js-search').on('click', function(e) {
+  $('#search').on('input', function(e) {
     e.preventDefault();
     const keyword = $('#search').val()
     const search_type = $('#search_type').val()
@@ -17,6 +69,23 @@ $(function() {
     })
       
     .done(function (results) {
+      $('.search_result').empty();
+      let addHTML = ''
+
+      if (results.articles.length > 0) { 
+        results.articles.forEach(function (article) {
+          addHTML += buildArticle(article);
+        });
+      }
+      
+      if (results.questions.length > 0) {   
+        results.questions.forEach(function (question) {
+          addHTML += buildQuestions(question);
+        });
+      } 
+      
+      $('.search_result').append(addHTML);
+
       console.log(results)
     })
   })
--- app/views/search/index.json.jbuilder
+++ app/views/search/index.json.jbuilder
@@ -1,17 +1,9 @@
 json.articles @articles do |article|
-  json.extract! article, :title
-  json.user_name article.user.nickname
+  json.partial!(article)
 end
-
-
 
 json.questions @questions do |question|
-  json.extract! question, :title, :created_at
+  json.extract! question, :title, :id
+  json.created_at question.created_at.strftime("%Y-%m-%d %H:%M")
   json.user_name question.user.nickname
-end
-
-#以下次回修正
-#@tagsが配列のなかに配列が入っている、それをjbuilderのなかで表現方法が
-json.array! @tags do |tag|
-  json.name tag.name
 end
