86b30894a84b56cffcdcb082bb744fb1158bf6b3
marina-kasahara <marina.kasahara611@gmail.com>
2020-03-15 16:24:15 +0900

    Update favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,24 @@
 class FavoritesController < ApplicationController
   before_action :set_article, only: :create
+  protect_from_forgery except: :create
 
   def index
     @favorites = current_user.favorite_articles
   end
 
   def create
+    binding.pry
     @favorite = current_user.favorites.new(article: @article)
+   
     if @favorite.save
-      redirect_to @article
+      respond_to do |format|
+        format.html {redirect_to @article}
+        format.json
+      end
     else
       render template: "articles/show"
     end
   end
-
-  # TODO：
-  # JSでお気に入りボタン押されたら非同期でここに飛ばす
 
   private
   def set_article
