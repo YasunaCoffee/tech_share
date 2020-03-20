13fa2cb1f89f9cebfa47cfab2260fd76081815b7
marina-kasahara <marina.kasahara611@gmail.com>
2020-03-20 16:37:16 +0900

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,8 +5,4 @@ class Article < ApplicationRecord
   has_many :tags, through: :article_tags
   belongs_to :user
   has_many :favorites, dependent: :destroy
-
-  def favorited?(user)
-    favorites.where(user_id: user.id).exists?
-  end
 end
