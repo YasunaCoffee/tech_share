2fe144f8589633edf0fa0bf7103eef06d9ffbbe7
Kazushi Yamanishi <keironia187149@gmail.com>
2020-03-26 16:38:34 +0900

    add default url option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'https://tech-share.work' }
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
     address: "smtp.gmail.com",
