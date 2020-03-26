023456cb9063a9251400f545254ffc0e22db775d
Kazushi Yamanishi <keironia187149@gmail.com>
2020-03-26 16:24:17 +0900

    add mail setting on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'smtp.gmail.com',
+    user_name:  Rails.application.credentials.mail[:user_name],
+    password: Rails.application.credentials.mail[:password],
+    authentication: 'login',
+  }
 end
