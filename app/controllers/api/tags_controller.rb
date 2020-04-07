ec1836457dc946a885c54ec4d2b714fca41414bc
Kazushi Yamanishi <keironia187149@gmail.com>
2020-04-07 15:11:16 +0900

    add tags create by ajax

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,6 +1,21 @@
 class Api::TagsController < ApplicationController
+
   def search
     @tags = Tag.where('name LIKE(?)', "#{params[:keyword]}%").where.not(id: params[:tag_ids])
     render json: @tags
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      render json: @tag
+    else
+      render json: @tag
+    end
+  end
+
+  private
+  def tag_params
+    params.permit(:name)
   end
 end
