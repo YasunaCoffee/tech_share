8504d69405696ea99514093a4947d678d288eb9d
Yasuna Miura <yasuna.miura@di-v.co,.jp>
2020-04-29 17:32:25 +0900

    change-markdown-css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,8 @@ module ApplicationHelper
 
   class HTMLwithCoderay < Redcarpet::Render::HTML
     def block_code(code, language)
-    #   language = language.split(':')[0]
-      case language.to_s
+        language = language.split(':')[0] if language.present?
+    case language.to_s
       when 'rb'
           lang = 'ruby'
       when 'yml'
@@ -18,14 +18,25 @@ module ApplicationHelper
           lang = 'md'
       else
           lang = language
-      end
-
+    end
       CodeRay.scan(code, lang).div
-      end
+  end
+    def list(text,type)
+        %(<ol class="ol-markdown">#{text}</ol>)
+    end
+
+    def list_item(text, list_type)
+        %(<li class="li-markdown">#{text}</li>)
+    end
+
+    def table(header, body)
+        %(<table class="table-markdown"><thead class="thead-markdown"><tr class="tr-markdown">#{header}<tr></thead><tbody class="tbody-markdown">#{body}</tbody></table>)
+    end
   end
 
+
   def markdown(text)
-      html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
+      html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true,  link_attributes: { rel: 'nofollow', target: "_blank" })
       options = {
           autolink: true,
           space_after_headers: true,
@@ -35,9 +46,13 @@ module ApplicationHelper
           hard_wrap: true,
           xhtml: true,
           lax_html_blocks: true,
-          strikethrough: true
+          strikethrough: true,
+          underline: true,
+          highlight: true,
+          list: true
       }
       markdown = Redcarpet::Markdown.new(html_render, options)
+    #   markdown = Redcarpet::Markdown.new(CustomRender, list: true)
       markdown.render(text)
   end
 end
